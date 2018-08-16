7524132bc8fe79faa9dd38f419a1d77e8da43418
Natalia <nataliamh.88@gmail.com>
2018-08-16 11:48:19 -0300

    reciclado de food map anterior

---
+++ src/js/view/splash.js
@@ -0,0 +1,11 @@
+window.onload=timeout;
+function timeout() {
+    window.setTimeout("redirect()", 5000) // 5 segundos
+}
+
+function redirect() {
+    window.location="main.html"
+        return
+}
+
+
